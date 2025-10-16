564a2e4c78852561e7ed795caaf1ad3ce6910133
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-16 12:58:12 +0200

    Create 27.py

---
+++ 27.py
@@ -0,0 +1,23 @@
+numero = int(input("Introduce un numero para multiplicar: "))
+
+Tabla1 = numero * 1
+Tabla2 = numero * 2
+Tabla3 = numero * 3
+Tabla4 = numero * 4
+Tabla5 = numero * 5
+Tabla6 = numero * 6
+Tabla7 = numero * 7
+Tabla8 = numero * 8
+Tabla9 = numero * 9
+Tabla10 = numero * 10
+
+print (f' numero multiplicado por 1 = {Tabla1}')
+print (f' numero multiplicado por 2 = {Tabla2}')
+print (f' numero multiplicado por 3 = {Tabla3}')
+print (f' numero multiplicado por 4 = {Tabla4}')
+print (f' numero multiplicado por 5 = {Tabla5}')
+print (f' numero multiplicado por 6 = {Tabla6}')
+print (f' numero multiplicado por 7 = {Tabla7}')
+print (f' numero multiplicado por 8 = {Tabla8}')
+print (f' numero multiplicado por 9 = {Tabla9}')
+print (f' numero multiplicado por 10 = {Tabla10}')
