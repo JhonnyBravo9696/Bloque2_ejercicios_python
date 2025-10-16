8ec0503ae64f6c223aa8ac354ffc83f51372c4c1
JhonnyBravo9696 <jesuscortesnavarro1705@gmail.com>
2025-10-16 12:47:25 +0200

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,7 @@
+opciones = input('escribe salir para salir del bucle de preguntas: ')
+while opciones != 'salir':
+  if opciones == 'salir':
+    print ('salida')
+  else:
+    print ('bucle')
+  opciones = input('escribe salir para salir del bucle de preguntas: ')
